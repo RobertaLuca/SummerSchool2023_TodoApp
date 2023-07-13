7c663c64610bdf9bbd99d9d9a9d5596c83fba265
Roberta Luca <roberta.luca@student.unitbv.ro>
2023-07-13 11:56:49 +0300

    View model changes

--- TodoApp/TodoApp/ViewModels/TodoItemsViewModel.cs
+++ TodoApp/TodoApp/ViewModels/TodoItemsViewModel.cs
@@ -1,11 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using TodoApp.Models;
 
 namespace TodoApp.ViewModels;
 
-public sealed partial class TodoItemsViewModel : ViewModelBase
+public sealed partial class TodoItemsViewModel : ViewModelBase, INotifyPropertyChanged
 {
+    private IList<TodoItem> _todoItems = new List<TodoItem>();
+
     public string Greeting => "Welcome to Avalonia!";
 
     public TodoItem Item => new TodoItem("test", "desc", DateOnly.FromDateTime(DateTime.Now));
+
+    public IList<TodoItem> TodoItems
+    {
+        get
+        {
+            return _todoItems;
+        }
+
+        set
+        {
+            _todoItems = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    private void AddTodoItem(TodoItem item)
+    {
+        TodoItems.Add(item);
+    }
+
+    private void RemoveTodoItem(TodoItem item)
+    {
+        TodoItems.Remove(item);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
