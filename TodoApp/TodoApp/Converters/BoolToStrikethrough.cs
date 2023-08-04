b07d7d60b65c4a13a289c3a7bb22d65f8ce8e8a4
Dinu Alin <dinui.alin@gmail.com>
2023-08-04 08:45:15 +0300

    Something

---
+++ TodoApp/TodoApp/Converters/BoolToStrikethrough.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System.Globalization;
+
+namespace TodoApp.Converters
+{
+    public class BoolToStrikethrough : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool myBoolean)
+            {
+                if (myBoolean == true)
+                {
+                    return TextDecorations.Strikethrough;
+                }
+            }
+
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
