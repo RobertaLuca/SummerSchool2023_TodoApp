8bbc2083c0f76a9587cc123f3461b9c74c154bef
Andrei Maroiu <achimaroiu@yahoo.com>
2023-10-31 12:01:14 +0200

    added navigation to chat window

--- TodoApp/TodoApp/ViewModels/ChatViewModel.cs
+++ TodoApp/TodoApp/ViewModels/ChatViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Core;
 using TodoApp.Models;
 using TodoApp.Services;
 
@@ -7,15 +8,20 @@ namespace TodoApp.ViewModels;
 
 public sealed partial class ChatViewModel : ViewModelBase
 {
+	private readonly NavigationService _navigationService;
+	private readonly PageService _pageService;
 
 	[ObservableProperty]
 	private string _chatResponse = string.Empty;
 
-	public ChatViewModel(CurrentTodoService currentTodoService, IChatBotService chatBotService)
+	public ChatViewModel(CurrentTodoService currentTodoService, IChatBotService chatBotService, NavigationService navigationService, PageService pageService)
 	{
 		TodoItem = currentTodoService.CurrentTodo;
         ChatBotService = chatBotService;
-    }
+
+		_navigationService = navigationService;
+		_pageService = pageService;
+	}
 
 	public TodoItem? TodoItem { get; }
     public IChatBotService ChatBotService { get; }
@@ -27,5 +33,11 @@ public sealed partial class ChatViewModel : ViewModelBase
 		{
 			ChatResponse = await ChatBotService.AskAdvice(TodoItem.Title, TodoItem.Description);
 		}
+	}
+
+	[RelayCommand]
+	private void GoBack()
+	{
+		_navigationService.CurrentPageData = _pageService.Pages[typeof(TodoItemsViewModel)];
 	}
 }
